c29efd1342eaed2775987c4a1db0930e69771813
joao guimaraes <guimaraesjoao101@gmail.com>
2025-06-10 17:33:21 -0300

    adciona verificação via hash MD5 para proteger resposta do desafio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-
 function checkMorseAnswer() {
     const answerInput = document.getElementById('morse-answer');
     const feedback = document.getElementById('morse-feedback');
-
     const answer = answerInput.value.trim().toLowerCase();
-    const correct = "codifique suas ideias";
 
     if (!answer) {
         feedback.textContent = "Digite uma resposta antes de verificar!";
@@ -14,28 +11,34 @@ function checkMorseAnswer() {
     }
 
     const normalize = text =>
-        text.normalize("NFD")
-            .replace(/\p{Diacritic}/gu, "")
-            .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
-            .replace(/\s+/g, " ")
-            .trim()
-            .toLowerCase();
+    text.normalize("NFD")
+        .replace(/\p{Diacritic}/gu, "");
 
-    if (normalize(answer) === normalize(correct)) {
+    const entradaNormalizada = normalize(answer);
+    const hashEntrada = CryptoJS.MD5(entradaNormalizada).toString();
+
+
+    const hashCorreto = "9a334330941d0051d5b25848c651b6f4";
+
+    if (hashEntrada === hashCorreto) {
         feedback.textContent = "Parabéns, está correto!";
         feedback.className = "feedback success";
         feedback.style.display = "block";
-
+        
         setTimeout(() => {
             document.getElementById('morse-challenge').classList.add('hidden');
             document.getElementById('final-result').classList.remove('hidden');
             window.scrollTo(0, 0);
-        }, 1500);
+        }, 800);
     } else {
         feedback.textContent = "Hmm, não parece correto. Tente novamente!";
         feedback.className = "feedback error";
         feedback.style.display = "block";
     }
+
+
+    console.log(entradaNormalizada);
+    console.log(hashEntrada);
 }
 
 function toggleMorseHelp() {
